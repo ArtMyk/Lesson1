bdbdaf039669f3c762b56953b1c25f0542872d71
mykla <artem.myklashchuk@cmail.com>
2024-02-11 15:39:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# number3 = int(input("Enter third number: "))
+#
+#
+# addition = number1 + number2 + number3
+# print (f"number1: {number1} number2: {number2} number3: {number3}")
+# print (f"Addition: {addition}")
+#
+#
+# multiplication = number1 * number2 * number3
+# print (f"number1: {number1} number2: {number2} number3: {number3}")
+# print (f"Multiplication: {multiplication}")
+#
+#
+# lenghtAB = int(input("Enter first number: "))
+# lenghtCD = int(input("Enter second number: "))
+# area = ((lenghtAB * lenghtCD)/2)
+# print (f"lenghtAB: {lenghtAB} lenghtCD: {lenghtCD}")
+# print (f"area: {area}")
+#
+# number = input("Enter for digit number: ")
+#
+# multiplication = int(number[0]) * int(number[1]) * int(number[2]) * int(number[3])
+# print (f"multiplication_number", multiplication)
+
+#################################################
+
+n1, n2 = 10, 20
+print(n1 > n2)
