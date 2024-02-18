27e415b7b871c5244ed61cad436e1608ed816925
mykla <artem.myklashchuk@cmail.com>
2024-02-18 17:23:21 +0100

    Homework_3_task3 completed

--- Homework_3.py
+++ Homework_3.py
@@ -24,18 +24,48 @@
 
 #############################################
 
+# try:
+#     number = int(input("Enter two numbers: "))
+#
+#     n1 = number // 10
+#     n2 = number // 1 % 10
+#
+#     if n1 == n2:
+#         print("True")
+#     if n1 > n2:
+#         print(f"n2 smaler: {n2}, {n1}")
+#     if n1 < n2:
+#         print(f"n1 smaler: {n1}, {n2}")
+#
+# except Exception as e:
+#     print(f"Error: {e}")
+
+###################################################
+
+
 try:
     number = int(input("Enter two numbers: "))
 
     n1 = number // 10
     n2 = number // 1 % 10
 
-    if n1 == n2:
-        print("True")
-    if n1 > n2:
-        print(f"n2 smaler: {n2}, {n1}")
-    if n1 < n2:
-        print(f"n1 smaler: {n1}, {n2}")
+    print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division")
+    user_select = int(input("Mathematical operation: "))
+
+    if user_select == 1:
+        addition_result = n1 + n2
+        print(f"Result: {addition_result} ")
+    elif user_select == 2:
+        subtraction_result = n1 - n2
+        print(f"Result: {subtraction_result} ")
+    elif user_select == 3:
+        multiplication_result = n1 * n2
+        print(f"Result: {multiplication_result} ")
+    elif user_select == 4:
+        division_result = n1 / n2
+        print(f"Result: {division_result} ")
+    else:
+        print("Enter correct mathematical operation number!")
 
 except Exception as e:
     print(f"Error: {e}")
